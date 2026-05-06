501a65653a6a04bbe0a1eb87561f774446241580
Salik754 <salikahmadn505@deltalearns.ca>
2026-05-06 09:35:55 -0700

    psuedocode
    yh

---
+++ main.py
@@ -0,0 +1,12 @@
+# Function
+        # Category
+        # Extra message if checkbox checked
+# ---- Widgets ----
+# Mass
+# Height
+# Radiobutton (Gender)
+# OptionMenu (Activity)
+# Checkbutton
+# Button
+# Output Label
+# Run
